0d57bdddf34ddfde13326925c0fd393db106693f
bagus2x <dutapanggulu@gmail.com>
2023-03-18 07:35:59 +0700

    delete comments

--- android/app/src/main/java/bagus2x/sosmed/presentation/common/media/DeviceMediaManager.kt
+++ android/app/src/main/java/bagus2x/sosmed/presentation/common/media/DeviceMediaManager.kt
@@ -32,6 +32,7 @@ interface DeviceMediaManager {
     fun getImagesAndVideos(pageSize: Int): Flow<PagingData<DeviceMedia>>
 
     fun getImages(pageSize: Int): Flow<PagingData<DeviceMedia.Image>>
+
     fun getVideos(pageSize: Int): Flow<PagingData<DeviceMedia.Video>>
 }
 
@@ -71,30 +72,20 @@ class DeviceMediaManagerImpl internal constructor(
                 params: LoadParams<Int>
             ): LoadResult<Int, T> {
                 try {
-                    // Start refresh at page 1 if undefined.
                     val nextPageNumber = params.key ?: 1
                     val deviceMedias = factory(params.loadSize, nextPageNumber)
                     return LoadResult.Page(
                         data = deviceMedias,
-                        prevKey = null, // Only paging forward.
+                        prevKey = null,
                         nextKey = if (deviceMedias.isNotEmpty()) nextPageNumber + 1 else null
                     )
                 } catch (e: Exception) {
-                    // Handle errors in this block and return LoadResult.Error if it is an
-                    // expected error (such as a network failure).
                     LoadResult.Error<Int, T>(e)
                     error(e)
                 }
             }
 
             override fun getRefreshKey(state: PagingState<Int, T>): Int? {
-                // Try to find the page key of the closest page to anchorPosition, from
-                // either the prevKey or the nextKey, but you need to handle nullability
-                // here:
-                //  * prevKey == null -> anchorPage is the first page.
-                //  * nextKey == null -> anchorPage is the last page.
-                //  * both prevKey and nextKey null -> anchorPage is the initial page, so
-                //    just return null.
                 return state.anchorPosition?.let { anchorPosition ->
                     val anchorPage = state.closestPageToPosition(anchorPosition)
                     anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
