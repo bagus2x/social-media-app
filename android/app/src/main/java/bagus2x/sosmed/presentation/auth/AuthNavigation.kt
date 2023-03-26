d9c7f38be4766d3e84b473337cbd09227249d04b
bagus2x <dutapanggulu@gmail.com>
2023-03-27 06:21:05 +0700

    add exitTransition and popEnterTransition

--- android/app/src/main/java/bagus2x/sosmed/presentation/auth/AuthNavigation.kt
+++ android/app/src/main/java/bagus2x/sosmed/presentation/auth/AuthNavigation.kt
@@ -1,10 +1,13 @@
 package bagus2x.sosmed.presentation.auth
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
 import androidx.hilt.navigation.compose.hiltViewModel
 import bagus2x.sosmed.presentation.auth.signin.SignInScreen
 import bagus2x.sosmed.presentation.auth.signup.SignUpScreen
 import bagus2x.sosmed.presentation.common.Destination
 
+@OptIn(ExperimentalAnimationApi::class)
 object SignUpScreen : Destination(
     authority = "sign_up",
     screen = { _, navHostController ->
@@ -12,9 +15,22 @@ object SignUpScreen : Destination(
             navController = navHostController,
             viewModel = hiltViewModel()
         )
+    },
+    enterTransition = {
+        slideIntoContainer(AnimatedContentScope.SlideDirection.Left, animationSpec = tween(500))
+    },
+    exitTransition = {
+        scaleOut() + fadeOut()
+    },
+    popEnterTransition = {
+        scaleIn() + fadeIn()
+    },
+    popExitTransition = {
+        slideOutOfContainer(AnimatedContentScope.SlideDirection.Right, animationSpec = tween(500))
     }
 )
 
+@OptIn(ExperimentalAnimationApi::class)
 object SignInScreen : Destination(
     authority = "sign_in",
     screen = { _, navHostController ->
@@ -22,6 +38,18 @@ object SignInScreen : Destination(
             navController = navHostController,
             viewModel = hiltViewModel()
         )
+    },
+    enterTransition = {
+        slideIntoContainer(AnimatedContentScope.SlideDirection.Left, animationSpec = tween(500))
+    },
+    exitTransition = {
+        scaleOut() + fadeOut()
+    },
+    popEnterTransition = {
+        scaleIn() + fadeIn()
+    },
+    popExitTransition = {
+        slideOutOfContainer(AnimatedContentScope.SlideDirection.Right, animationSpec = tween(500))
     }
 )
 
